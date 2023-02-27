4c1b485561ee999ff9f26003d6537064e8858a84
Daniela <ciucur.daniela@gmail.com>
2023-02-27 01:29:29 +0100

    added docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Parcel } from './parcels/models/parcel.entity';
-import * as typeOrmConfig from './typeorm.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ParcelsModule } from './parcels/parcels.module';
 
 @Module({
   imports: [
     ParcelsModule,
-    TypeOrmModule.forRoot(typeOrmConfig),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [Parcel],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
